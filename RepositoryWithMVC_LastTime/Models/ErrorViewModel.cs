825e2a85bd9259120631d4b47888548cac3fe8cf
YanjunYu-1 <y9y9j8@163.com>
2022-05-18 15:40:57 -0500

    添加项目文件。

---
+++ RepositoryWithMVC_LastTime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositoryWithMVC_LastTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
